4c7314baa678920308fc5fd59d3ac2329da6e3e7
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-05-13 23:42:58 +0300

    /start command done

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,7 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
+from aiogram import types
+from aiogram.filters import Command
 import os
 
 import logging
@@ -11,6 +13,11 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def start_command_handler(message: types.Message):
+    logging.info('Start command received')
+    await message.answer('Привет! Я бот-помощник сообщества EESTEC LC St. Petersburg!')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
