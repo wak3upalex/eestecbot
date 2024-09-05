364bcefd16eaa459a88e9277f67a3d07ab367612
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-09-05 13:03:20 +0300

    Add everything from `bot.py` to include `/start` comand handler

--- bot/inLineButtons_quest.py
+++ bot/inLineButtons_quest.py
@@ -8,6 +8,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import KeyboardButton, ReplyKeyboardRemove
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from bot import *
 from bot import bot, dp
 
 
